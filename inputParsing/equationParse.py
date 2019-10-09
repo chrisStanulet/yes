92f14d15e3cc00355a7b9c4e747e59aa72c77bc3
Angelo DeLuca <angelo.deluca.2022@kenstonapps.org>
2019-10-09 00:18:20 -0400

    Fixed a few errors in algorithm and added pseudocode comment.
    Signed-off-by: Angelo DeLuca <angelo.deluca.2022@kenstonapps.org>

--- inputParsing/equationParse.py
+++ inputParsing/equationParse.py
@@ -1,8 +1,9 @@
 operators = ['+', '-', '*', '/', '^']
-validChars = ['.', '|', '(',')']
+validChars = ['.', '|', '(', ')']
 functions = ["sin", "cos", "tan", "sqrt", "asin", "acos", "atan"]
 
 
+# Example: 16-2x=5x+9
 def parse(e):
     outputQueue = []
     operatorStack = []
@@ -15,14 +16,24 @@ def parse(e):
         for char in exp:
             if char.isnumeric():  # if token is a number...
                 outputQueue.append(char)  # push to output queue
-            else:
-                if iterations + 3 < len(exp) - 1:
-                    if contains(exp[iterations:iterations + 3], functions):
-                        print("Work In progress...")
+            else:  # if token is not a number
+
+                # if token is a function
+                if iterations + 3 < len(exp) - 1 and contains(exp[iterations:iterations + 3], functions):
+                    operatorStack.append(exp[iterations:iterations + 3])
+                elif iterations + 4 < len(exp) - 1 and contains(exp[iterations:iterations + 4], functions):
+                    operatorStack.append(exp[iterations:iterations + 4])
+
+                # if token is an operator
+                if contains(char, operators):
+                    '''while ((there is a function at the top of the operator stack)
+               or (there is an operator at the top of the operator stack with greater precedence)
+               or (the operator at the top of the operator stack has equal precedence and is left associative))
+              and (the operator at the top of the operator stack is not a left parenthesis):
+            pop operators from the operator stack onto the output queue.
+        push it onto the operator stack.'''
 
         iterations += 1
-
-
 
 
 def contains(a, b):
@@ -30,7 +41,7 @@ def contains(a, b):
     i = 0
     for char in a:
         if char in b:
-            res +=1
+            res += 1
         i += 1
 
     if res > 0:
@@ -53,10 +64,7 @@ def replace(a, b, c):
     return res
 
 
-
-
-
-#TESTING
+# TESTING
 equation = input("Enter Equation\n")
 
 parse(equation)
