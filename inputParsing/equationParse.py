d1c356bd8680ac7c7958d187896191ecee52b722
Angelo DeLuca <angelo.deluca.2022@kenstonapps.org>
2019-10-11 19:41:48 -0400

    Parsing algorithm almost done.
    Added tensorflow to natlang.py
    
    Signed-off-by: Angelo DeLuca <angelo.deluca.2022@kenstonapps.org>

--- inputParsing/equationParse.py
+++ inputParsing/equationParse.py
@@ -3,10 +3,12 @@ validChars = ['.', '|', '(', ')']
 functions = ["sin", "cos", "tan", "sqrt", "asin", "acos", "atan"]
 
 
+
 # Example: 16-2x=5x+9
+outputQueue = []
+operatorStack = []
+precedence = {'(':1,'^':2,'m':3,'/':4,'+':5,'-':6}
 def parse(e):
-    outputQueue = []
-    operatorStack = []
 
     expressions = e.split('=')
 
@@ -14,26 +16,70 @@ def parse(e):
 
     for exp in expressions:
         for char in exp:
-            if char.isnumeric():  # if token is a number...
-                outputQueue.append(char)  # push to output queue
-            else:  # if token is not a number
 
-                # if token is a function
-                if iterations + 3 < len(exp) - 1 and contains(exp[iterations:iterations + 3], functions):
-                    operatorStack.append(exp[iterations:iterations + 3])
-                elif iterations + 4 < len(exp) - 1 and contains(exp[iterations:iterations + 4], functions):
-                    operatorStack.append(exp[iterations:iterations + 4])
+            #If the character is numeric, push to output queue.
+            if char.isnumeric():
+                outputQueue.append(char)
+                exp.pop(iterations)
 
-                # if token is an operator
-                if contains(char, operators):
-                    '''while ((there is a function at the top of the operator stack)
-               or (there is an operator at the top of the operator stack with greater precedence)
-               or (the operator at the top of the operator stack has equal precedence and is left associative))
-              and (the operator at the top of the operator stack is not a left parenthesis):
-            pop operators from the operator stack onto the output queue.
-        push it onto the operator stack.'''
+            #If the character is an operator...
+            if char in operators:
 
-        iterations += 1
+                #While there is an operator on top of the operator stack, there is an operator at the top of the operator stack with greater precedence, or the operator at the top of the operator stack has equal precedence and is left associative
+                while (len(operatorStack) > 0 or hasGreaterPrecedence(char, operatorStack(len(operatorStack)-1)) == 1 or (hasGreaterPrecedence(char, operatorStack(len(operatorStack)-1)) == 0 and operatorStack(len(operatorStack)-1) == '{')) and operatorStack(len(operatorStack)-1) != '(':
+
+                    outputQueue.append(operatorStack.pop(len(operatorStack)-1))
+
+                operatorStack.append(char)
+                exp.pop(iterations)
+
+            if char == '(':
+                operatorStack.append('(')
+                exp.pop(iterations)
+
+            if char == ')':
+                while operatorStack(len(operatorStack)-1) != '(':
+                    outputQueue.append(operatorStack.pop(len(operatorStack)-1))
+
+                if operatorStack(len(operatorStack)-1) == ')':
+                    operatorStack.pop(len(operatorStack)-1)
+
+            iterations += 1
+
+        if operatorStack is not None:
+
+
+
+
+
+
+def popAll(a, popToA):
+    i = 0
+    for char in popToA:
+        a.append(a.ppend)
+
+        i+=1
+
+
+
+def hasGreaterPrecedence(a,b):
+
+    if precedence[a] > precedence[b]:
+        return 1
+    else:
+        return -1
+
+
+
+
+def isFunctionOnTop():
+
+    for funct in functions:
+        if operatorStack[len(operatorStack)-1] in funct:
+            return True
+        else:
+            return False
+
 
 
 def contains(a, b):
