dadbf4e8cdb86e9aae200cb5a34121b86b6966fc
chrisStanulet <43823693+chrisStanulet@users.noreply.github.com>
2019-10-19 10:55:23 -0400

    its something

--- inputParsing/equationParse.py
+++ inputParsing/equationParse.py
@@ -1,76 +1,22 @@
-operators = ['+', '-', '*', '/', '^']
-validChars = ['.', '|', '(',')']
+def parse(x):
+    x = x.replace(" ", "")
+    operators = set('+-*/()')
+    numbers = set("123456789")
+    op_out = []  # This holds the operators that are found in the string (left to right)
+    num_out = []  # this holds the non-operators that are found in the string (left to right)
+    waste = []
 
-
-
-def parse(e):
-    result = ''
-    # Split solve
-    e = e.replace(' ','')
-    e = e.split('=')
-    if len(e) != 2:
-        return 'ERR_2EQUAL'
-
-    eqobj = [{}]
-
-    #Simplify
-    # Example: 16-2x=5x+9
-    start = 0
-    stop = 0
-    i = 0
-    expPart = 0
-    for exp in e:
-        for c in exp:
-            if contains(c,operators): # If current char is an operator (Means that we have either found a new value, or the value is at the beginning, and is negative
-                if i > 0: # Add value to equation object when not at first pause.
-                    e = {'val':exp[start:stop]}
-                    eqobj.append(e.copy())
-                    if exp[i-1].isalpha(): # if character before operator is not a number (e.g. a variable,) bind variable to value.
-                        eqobj[len(eqobj)-1]['multBy'] = exp[i-1]
-                start = i
-            else:
-                stop = i+1
-            i +=1
-    expPart+=1
-
-    print(expPart)
-
-
-
-def contains(a, b):
-    res = 0
-    i = 0
-    for char in a:
-        if char in b:
-            res +=1
-        i += 1
-
-    if res > 0:
-        return True
-    else:
-        return False
-
-
-def replace(a, b, c):
-    res = ''
-    i = 0
-    for char in a:
-        if char in b:
-            res += c
+    for c in x:  # examine 1 character at a time
+        if c in operators:
+            op_out.append(c)
+            num_out.append("")#append space to show where an operator should be in an equation. Makes it easier if beginning number is negative. Or +(-x)
+        elif c in numbers:
+            num_out.append(c)
         else:
-            res += a[i]
-
-        i += 1
-
-    return res
+            # not an operator or number. Useless trash.
+            waste.append(c)
+    return num_out, op_out
 
 
+print(parse('-3/2*15'))
 
-
-
-#TESTING
-equation = input("Enter Equation\n")
-
-parse(equation)
-
-# parse(equation)
