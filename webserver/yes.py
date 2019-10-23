7170e4d03e1aa36dd67aa85b7d881b34ae67c0b8
Angelo DeLuca <angelo.deluca.2022@kenstonapps.org>
2019-10-22 21:48:42 -0400

    Started work for the freaking webserver.
    Signed-off-by: Angelo DeLuca <angelo.deluca.2022@kenstonapps.org>

--- webserver/yes.py
+++ webserver/yes.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 import inputParsing.equationParse as rpn
 
@@ -10,5 +11,14 @@ def main():
     return rpn.rpnToString(rpn.shuntingYardAlgorithm('( ( 15 / ( 7 - ( 1 + 1 ) ) ) * 3 ) - ( 2.4 + ( 1 + 1 ) )'))
 
 
+@app.route("/api/infixNotation")
+def infix():
+    if 'q' not in request.args:
+        return "you absolute fool... enter a query with the parameter 'q'!"
+    return rpn.rpnToString(rpn.shuntingYardAlgorithm(request.args['q']))
+
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", potrt=80)
+    app.run(debug=True, host="0.0.0.0", port=80)
